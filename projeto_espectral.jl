5cf1457890c15d9813d37ed3467083c22ba8e761
felipegin <felipebbgin@gmail.com>
2024-11-03 20:34:53 -0300

    Fazendo ajustes no Armijo2 e organizando o código

--- projeto_espectral.jl
+++ projeto_espectral.jl
@@ -17,77 +17,54 @@ mutable struct metrics
     gradient_norm_value::Float64
     function_count::Int
     gradient_count::Int
+    iteracoes::Int
+    tempo::Float64
 end
 
 # Criando DataFrame para armazenar os resultados
 results_df = DataFrame(
-    Problem = String[], 
-    f_Cauchy = Union{Float64, Missing}[], grad_Cauchy = Union{Float64, Missing}[], 
-    f_count_Cauchy = Union{Int, Missing}[], grad_count_Cauchy = Union{Int, Missing}[],
-    f_BFGS = Union{Float64, Missing}[], grad_BFGS = Union{Float64, Missing}[], 
-    f_count_BFGS = Union{Int, Missing}[], grad_count_BFGS = Union{Int, Missing}[],
-    f_Spectral = Union{Float64, Missing}[], grad_Spectral = Union{Float64, Missing}[], 
-    f_count_Spectral = Union{Int, Missing}[], grad_count_Spectral = Union{Int, Missing}[],
-    f_Spectral2 = Union{Float64, Missing}[], grad_Spectral2 = Union{Float64, Missing}[], 
-    f_count_Spectral2 = Union{Int, Missing}[], grad_count_Spectral2 = Union{Int, Missing}[],
-    f_Spectral3 = Union{Float64, Missing}[], grad_Spectral3 = Union{Float64, Missing}[], 
-    f_count_Spectral3 = Union{Int, Missing}[], grad_count_Spectral_armijo3 = Union{Int, Missing}[]
+    Problem = String[],
+    N_var = Union{Float64, Missing}[],
+    Densidade = Union{Float64, Missing}[],
+    Method = String[],
+    Gradiente = Union{Float64, Missing}[],
+    Funcao = Union{Float64, Missing}[],
+    Iteracoes = Int[],
+    Avaliacoes_grad = Union{Float64, Missing}[],
+    Avaliacoes_f = Union{Float64, Missing}[],
+    Max_iteracoes = Int[],
+    Fator_reducao = Float64[],  # Alterado para Float64
+    Passo_inicial_armijo = Float64[],  # Alterado para Float64
+    Parada_grad = Float64[],
+    Parada_armijo = Float64[],
+    Tempo = Union{Float64, Missing}[],
+    Memoria = Union{Float64, Missing}[]
 )
 
 
-function registro!(results_df, nlp, count::metrics, algoritmo::String)
+function registro!(results_df, nlp, count::metrics, algoritmo::String, iteracoes, fator_reducao, passo_inicial, parada_grad, parada_armijo, tempo, memoria)
     # Obter o nome do problema atual
     problem_name = nlp.meta.name
 
-    # Verificar se o DataFrame está vazio
-    if isempty(results_df)
-        # Se o DataFrame estiver vazio, cria uma nova linha diretamente
-        push!(results_df, (
-            Problem = problem_name,
-            f_Cauchy = missing, grad_Cauchy = missing, f_count_Cauchy = missing, grad_count_Cauchy = missing,
-            f_BFGS = missing, grad_BFGS = missing, f_count_BFGS = missing, grad_count_BFGS = missing,
-            f_Spectral = missing, grad_Spectral = missing, f_count_Spectral = missing, grad_count_Spectral = missing,
-            f_Spectral2 = missing, grad_Spectral2 = missing, f_count_Spectral2 = missing, grad_count_Spectral2 = missing,
-            f_Spectral3 = missing, grad_Spectral3 = missing, f_count_Spectral3 = missing, grad_count_Spectral_armijo3 = missing
-        ))
-        # Definir `row_index` como a última linha do DataFrame
-        row_index = nrow(results_df)
-    else
-        # Se o DataFrame não está vazio, buscar a linha correspondente
-        row_index = findfirst(row -> row[:Problem] == problem_name, eachrow(results_df))
-
-        # Se o problema não estiver no DataFrame, cria uma nova linha
-        if isnothing(row_index)
-            push!(results_df, (
-                Problem = problem_name,
-                f_Cauchy = missing, grad_Cauchy = missing, f_count_Cauchy = missing, grad_count_Cauchy = missing,
-                f_BFGS = missing, grad_BFGS = missing, f_count_BFGS = missing, grad_count_BFGS = missing,
-                f_Spectral = missing, grad_Spectral = missing, f_count_Spectral = missing, grad_count_Spectral = missing,
-                f_Spectral2 = missing, grad_Spectral2 = missing, f_count_Spectral2 = missing, grad_count_Spectral2 = missing,
-                f_Spectral3 = missing, grad_Spectral3 = missing, f_count_Spectral3 = missing, grad_count_Spectral_armijo3 = missing
-            ))
-            # Atualizar `row_index` para a última linha do DataFrame
-            row_index = nrow(results_df)
-        end
-    end
-
-    # Atualizar apenas as colunas específicas ao algoritmo atual
-    if algoritmo == "Cauchy"
-        results_df[row_index, [:f_Cauchy, :grad_Cauchy, :f_count_Cauchy, :grad_count_Cauchy]] = 
-            (count.function_value, count.gradient_norm_value, count.function_count, count.gradient_count)
-    elseif algoritmo == "BFGS"
-        results_df[row_index, [:f_BFGS, :grad_BFGS, :f_count_BFGS, :grad_count_BFGS]] = 
-            (count.function_value, count.gradient_norm_value, count.function_count, count.gradient_count)
-    elseif algoritmo == "Spectral"
-        results_df[row_index, [:f_Spectral, :grad_Spectral, :f_count_Spectral, :grad_count_Spectral]] = 
-            (count.function_value, count.gradient_norm_value, count.function_count, count.gradient_count)
-    elseif algoritmo == "Spectral_armijo"
-        results_df[row_index, [:f_Spectral2, :grad_Spectral2, :f_count_Spectral2, :grad_count_Spectral2]] = 
-            (count.function_value, count.gradient_norm_value, count.function_count, count.gradient_count)
-    elseif algoritmo == "Spectral_armijo2"
-        results_df[row_index, [:f_Spectral3, :grad_Spectral3, :f_count_Spectral3, :grad_count_Spectral_armijo3]] = 
-            (count.function_value, count.gradient_norm_value, count.function_count, count.gradient_count)
-    end
+    # Adiciona uma nova linha com todas as informações
+    push!(results_df, (
+        Problem = problem_name,
+        N_var = nlp.meta.nvar,
+        Densidade = nlp.meta.nnzh/(nlp.meta.nvar^2),
+        Method = algoritmo,
+        Gradiente = count.gradient_norm_value,
+        Funcao = count.function_value,
+        Iteracoes = iteracoes,
+        Avaliacoes_grad = count.gradient_count,
+        Avaliacoes_f = count.function_count,
+        Max_iteracoes = iteracoes,
+        Fator_reducao = fator_reducao,
+        Passo_inicial_armijo = passo_inicial,
+        Parada_grad = parada_grad,
+        Parada_armijo = parada_armijo,
+        Tempo = tempo,
+        Memoria = memoria
+    ))
 end
 
 #================================================================================================================
@@ -115,178 +92,159 @@ end
     - `criterio_parada` : a partir de qual valor da norma do gradiente o algoritmo pode parar
 =================================================================================================================#
 
-function Cauchy(nlp, x, d, passo_inicial, fator_reducao, count, criterio_parada_grad, criterio_parada_i)
-    i=0 
+# Função de busca de Armijo
+function armijo_search(nlp, x, d, passo_inicial, fator_reducao, η, count)
+    α = passo_inicial
+    k = 0
+    count.function_count += 2
+    while (obj(nlp, x + α * d) > obj(nlp, x) - η * α * d' * d) && k < 50
+        count.function_count += 2
+        α *= fator_reducao
+        k += 1
+    end
+    return α
+end
+
+# Função Cauchy com uso da função armijo_search
+function Cauchy(nlp, x, d, passo_inicial, fator_reducao, η, count, criterio_parada_grad, criterio_parada_i)
+    i = 0
     while (norm(d) > criterio_parada_grad) && (i < criterio_parada_i)
-    
         # Definindo o gradiente
-        d=-grad(nlp, x)
+        d = -grad(nlp, x)
         count.gradient_count += 1
 
-        # Critérios iniciais da Busca de Armijo
-        α= passo_inicial
-        c= fator_reducao
-        
-        count.function_count += 2
         # Busca de Armijo
-        while (obj(nlp, x+α*d) > obj(nlp, x)-c*α*d'*d)
-            count.function_count += 2
-            α = α*c
-        end
-    
+        α = armijo_search(nlp, x, d, passo_inicial, fator_reducao, η, count)
+
         # Atualização do ponto com a direção e o passo calculados
-        x=x+α*d
-    
-        i=i+1
-        println("--------- Iteração: ",i," ---------")
-        println("f(x) = ",obj(nlp, x))
-        println("|grad_f(x)| = ", norm(d))
-    
+        x = x + α * d
+        i += 1
+        count.iteracoes = i
     end
 
-    # Atualiando as métricas
+    println("f(x) = ", obj(nlp, x))
+    println("|grad_f(x)| = ", norm(d))
+    # Atualizando as métricas
     count.function_value = obj(nlp, x)
     count.gradient_norm_value = norm(d)
 end
 
-function BFGS(nlp, x, passo_inicial, fator_reducao, count, criterio_parada_grad, criterio_parada_i)
-    i=0
+# Função BFGS com uso da função armijo_search
+function BFGS(nlp, x, passo_inicial, fator_reducao, η, count, criterio_parada_grad, criterio_parada_i)
+    i = 0
     H = I(nlp.meta.nvar)
 
     g = grad(nlp, x)
     count.gradient_count += 1
-
-    d=-H*g
-
-
+    d = -H * g
+    ys_min = 0
+    ys = 1
 
     while (norm(g) > criterio_parada_grad) && (i < criterio_parada_i)
-    
         # Definindo o gradiente
-        d=-H*g
+        d = -H * g
         count.gradient_count += 1
 
-        # Critérios iniciais da Busca de Armijo
-        α= passo_inicial
-        c= fator_reducao
-        
-        count.function_count += 2
         # Busca de Armijo
-        while (obj(nlp, x+α*d) > obj(nlp, x)-c*α*d'*d)
-            count.function_count += 2
-            α = α*c
-        end
-    
+        α = armijo_search(nlp, x, d, passo_inicial, fator_reducao, η, count)
+
         # Atualização do ponto com a direção e o passo calculados
-        x1=x+α*d
-    
+        x1 = x + α * d
         g1 = grad(nlp, x1)
         count.gradient_count += 1
 
-        s=x1-x
-        y=g1-g
+        s = x1 - x
+        y = g1 - g
 
+        if !isnan(y' * s) && (y' * s) != 0.0
+            ys = (y' * s)
+            if ys != 0 && ys < ys_min
+                ys_min = ys
+            end
+        end
 
-        ρ=1/(y'*s)
-        V=I-ρ*(s*y')
-        H=V*H*V'+ρ*(s*s')
+        ρ = 1 / ys
+        V = I - ρ * (s * y')
+        H = V * H * V' + ρ * (s * s')
 
-        x=x1
-        g=g1
-
-        i=i+1
-        println("--------- Iteração: ",i," ---------")
-        println("f(x) = ",obj(nlp, x))
-        println("|grad_f(x)| = ", norm(d))
-        println("y'*s = ", y'*s)
-    
+        x = x1
+        g = g1
+        i += 1
+        count.iteracoes = i
     end
 
-    # Atualiando as métricas
+    println("f(x) = ", obj(nlp, x))
+    println("|grad_f(x)| = ", norm(d))
+    println("ys_min = ", ys_min)
+    # Atualizando as métricas
     count.function_value = obj(nlp, x)
     count.gradient_norm_value = norm(d)
 end
 
-function Spectral(nlp, x0, busca, passo_inicial, fator_reducao, count, criterio_parada_grad, criterio_parada_i)
-    i=0
-
-    g = grad(nlp,x0)
+# Função Spectral com uso da função armijo_search
+function Spectral(nlp, x0, busca, passo_inicial, fator_reducao, η, count, criterio_parada_grad, criterio_parada_i)
+    i = 0
+    g = grad(nlp, x0)
     count.gradient_count += 1
 
-    x1 = x0 + 0.001*g
-
-    g = grad(nlp,x1)
+    x1 = x0 - 0.01 * g
+    g = grad(nlp, x1)
     count.gradient_count += 1
-
     hist_f = [obj(nlp, x1)]
+    σ = 0.00001
 
     while (norm(g) > criterio_parada_grad) && (i < criterio_parada_i)
-        
-        δx = x1-x0
-        δy = grad(nlp, x1)-grad(nlp, x0)
+        δx = x1 - x0
+        δy = grad(nlp, x1) - grad(nlp, x0)
         count.gradient_count += 2
 
-        println((δx' * δx))
-
-        σ = (δx' * δy)/(δx' * δx)
-
-        g=grad(nlp, x1)
-        d=-g*(1/σ)
-        count.gradient_count += 1
-        
-        α = passo_inicial
-        c = fator_reducao
-
-        if busca == "Armijo"
-            k=0
-
-            count.function_count += 2
-            # Busca de Armijo
-            while (obj(nlp, x1+α*d) > obj(nlp, x1)-c*α*d'*d) && k<50
-                k+=1
-                count.function_count += 2
-                α = α*c
-            end
+        if ((δx' * δy) / (δx' * δx)) != 0 && !isnan((δx' * δy) / (δx' * δx)) || i == 0
+            σ = (δx' * δy) / (δx' * δx)
         end
 
-        if busca == "Armijo2"
-            k=0
+        g = grad(nlp, x1)
+        d = -g * (1 / σ)
+        count.gradient_count += 1
 
+        # Busca de Armijo
+        if busca == "Armijo"
+            α = armijo_search(nlp, x1, d, passo_inicial, fator_reducao, η, count)
+        elseif busca == "Armijo2"
+            k = 0
+            α = passo_inicial
             M = maximum(hist_f)
-
             count.function_count += 2
-            # Busca de Armijo
-            while (obj(nlp, x1+α*d) > M-c*α*d'*d) && k<50
-                k+=1
+            while (obj(nlp, x1 + α * d) > M - η * α * d' * d) && k < 50
+                k += 1
                 count.function_count += 2
-                α = α*c
+                α *= fator_reducao
             end
+        else
+            α = 1/σ
         end
 
         # Atualização do ponto com a direção e o passo calculados
-        x0=x1
-        x1=x0+α*d
-    
-        push!(hist_f, obj(nlp, x1))
+        x0 = x1
+        x1 = x0 + α * d
 
+        push!(hist_f, obj(nlp, x1))
         if length(hist_f) > 10
             popfirst!(hist_f)
         end
 
-        g = grad(nlp,x1)
-
-        i=i+1
-        println("--------- Iteração: ",i," ---------")
-        println("sigma = ",σ)
-        println("f(x) = ",obj(nlp, x1))
-        println("|grad_f(x)| = ", norm(g))
-    
+        g = grad(nlp, x1)
+        count.gradient_count += 1
+        i += 1
+        count.iteracoes = i
     end
 
-    # Atualiando as métricas
+    println("f(x) = ", obj(nlp, x1))
+    println("|grad_f(x)| = ", norm(g))
+    # Atualizando as métricas
     count.function_value = obj(nlp, x1)
     count.gradient_norm_value = norm(g)
 end
+
 
 
 tests = [
@@ -322,83 +280,58 @@ tests = [
     "TOINTGOR",
 ]
 
+# Parâmetros gerais
+passo_inicial = 1.0
+fator_reducao = 0.5
+parada_grad = 0.0001
+parada_armijo = 0.0001
+criterio_parada_i = 20000
+
+
 for test in tests
     nlp = CUTEstModel(test; decode=true)
-    println(test," importado!")
+    println(test, " importado!")
+
+    x0 = nlp.meta.x0
 
     # ------------------------- Cauchy -----------------------------
-    # Inicializar as métricas com zeros
-    count = metrics(0,0,0,0)
-
-    d = -grad(nlp,nlp.meta.x0)
-    count.gradient_count = 1
-
+#=
+    count = metrics(0.0, 0.0, 0, 0, 0, 0.0)
+    d = -grad(nlp, x0)
+    count.gradient_count += 1
     println("Começando Cauchy com ", test)
-    Cauchy(nlp, nlp.meta.x0, d, 1, 0.5, count1, 0.01, 1000)
+    memoria = @allocated count.tempo = @elapsed Cauchy(nlp, x0, d, passo_inicial, fator_reducao, parada_armijo, count, parada_grad, criterio_parada_i)
+    registro!(results_df, nlp, count, "Cauchy", count.iteracoes, fator_reducao, passo_inicial, parada_grad, parada_armijo, count.tempo, memoria)
 
-    # Armazenando métricas no DataFrame
-    println("Armazenando métricas")
-    registro!(results_df,nlp,count,"Cauchy")
-
+    
     # ------------------------- BFGS -----------------------------
-    # Inicializar as métricas com zeros
-    count = metrics(0,0,0,0)
-
-    d = -grad(nlp,nlp.meta.x0)
-    count.gradient_count = 1
-
+    count = metrics(0.0, 0.0, 0, 0, 0, 0.0)
     println("Começando BFGS com ", test)
-    BFGS(nlp, nlp.meta.x0, 0.5, 0.5, count2, 0.000001, 100)
-
-    # Armazenando métricas no DataFrame
-    println("Armazenando métricas")
-    registro!(results_df,nlp,count,"Cauchy")
-
+    memoria = @allocated  count.tempo = @elapsed BFGS(nlp, x0, passo_inicial, fator_reducao, parada_armijo, count, parada_grad, criterio_parada_i)
+    registro!(results_df, nlp, count, "BFGS", count.iteracoes, fator_reducao, passo_inicial, parada_grad, parada_armijo, count.tempo, memoria)
+=#
     # ------------------------- Spectral -----------------------------
-    # Inicializar as métricas com zeros
-    count = metrics(0,0,0,0)
 
-    d = -grad(nlp,nlp.meta.x0)
-    count.gradient_count = 1
-
+    count = metrics(0.0, 0.0, 0, 0, 0, 0.0)
     println("Começando Spectral com ", test)
-    Spectral(nlp, nlp.meta.x0, "", 1, 0.5, count, 0.01, 1000)
-
-    # Armazenando métricas no DataFrame
-    println("Armazenando métricas")
-    registro!(results_df,nlp,count,"Spectral")
+    memoria = @allocated count.tempo = @elapsed Spectral(nlp, x0, "", passo_inicial, fator_reducao, parada_armijo, count, parada_grad, criterio_parada_i)
+    registro!(results_df, nlp, count, "Spectral", count.iteracoes, fator_reducao, passo_inicial, parada_grad, parada_armijo, count.tempo, memoria)
 
     # ------------------------- Spectral Armijo -----------------------------
-    # Inicializar as métricas com zeros
-    count = metrics(0,0,0,0)
-
-    d = -grad(nlp,nlp.meta.x0)
-    count.gradient_count = 1
-
-    println("Começando Spectral2 com ", test)
-    Spectral(nlp, nlp.meta.x0, "Armijo", 1, 0.5, count, 0.01, 1000)
-
-    # Armazenando métricas no DataFrame
-    println("Armazenando métricas")
-    registro!(results_df,nlp,count,"Spectral_armijo")
+    count = metrics(0.0, 0.0, 0, 0, 0, 0.0)
+    println("Começando Spectral Armijo com ", test)
+    memoria = @allocated count.tempo = @elapsed Spectral(nlp, x0, "Armijo", passo_inicial, fator_reducao, parada_armijo, count, parada_grad, criterio_parada_i)
+    registro!(results_df, nlp, count, "Spectral_armijo", count.iteracoes, fator_reducao, passo_inicial, parada_grad, parada_armijo, count.tempo, memoria)
 
     # ------------------------- Spectral Armijo 2 -----------------------------
+    count = metrics(0.0, 0.0, 0, 0, 0, 0.0)
+    println("Começando Spectral Armijo 2 com ", test)
+    memoria = @allocated count.tempo = @elapsed Spectral(nlp, x0, "Armijo2", passo_inicial, fator_reducao, parada_armijo, count, parada_grad, criterio_parada_i)
+    registro!(results_df, nlp, count, "Spectral_armijo2", count.iteracoes, fator_reducao, passo_inicial, parada_grad, parada_armijo, count.tempo, memoria)
 
-    # Inicializar as métricas com zeros
-    count = metrics(0,0,0,0)
-
-    d = -grad(nlp,nlp.meta.x0)
-    count.gradient_count = 1
-
-    println("Começando Spectral3 com ", test)
-    Spectral(nlp, nlp.meta.x0, "Armijo2", 1, 0.5, count, 0.01, 1000)
-
-    # Armazenando métricas no DataFrame
-    println("Armazenando métricas")
-    registro!(results_df,nlp,count,"Spectral_armijo2")
 
     finalize(nlp)
-    println(test," finalizado!")
+    println(test, " finalizado!")
 end
 
 
@@ -407,4 +340,4 @@ println(results_df)
 Pkg.add("CSV")
 using CSV
 
-CSV.write("C:/Users/93dav/OneDrive/Documentos/Felipe/resultados_projeto_espectral/results_df.csv", results_df)
+CSV.write("C:/Users/93dav/OneDrive/Documentos/Felipe/resultados_projeto_espectral/results_Spectrals2.csv", results_df)
